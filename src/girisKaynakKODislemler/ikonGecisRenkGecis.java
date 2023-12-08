a633805121bd9ca8ee8e7373418230f43b708421
why20w18 <enco3162@gmail.com>
2023-12-08 17:36:17 +0300

    kayit ikonu getter ve objesi olusturuldu

--- src/girisKaynakKODislemler/ikonGecisRenkGecis.java
+++ src/girisKaynakKODislemler/ikonGecisRenkGecis.java
@@ -10,9 +10,14 @@ public class ikonGecisRenkGecis {
     private Icon user128pxGirisBeyaz = new ImageIcon(getClass().getResource("/imagesPack/user128pxGirisBeyaz.png"));
     private Icon user128pxGirisSiyah = new ImageIcon(getClass().getResource("/imagesPack/user128pxGirisSiyah.png"));
     private Icon user128pxGirisSifreUnuttun = new ImageIcon(getClass().getResource("/imagesPack/user128pxGirisSifreUnuttun.png"));
+    private Icon user128pxKayitOl = new ImageIcon(getClass().getResource("/imagesPack/user128pxKayitOl.png"));
     
     private Color sifreUnuttumMavi = new Color(102,153,255);
     private Color kayitOlMavi = new Color(51,51,255);
+
+    public Icon getUser128pxKayitOl() {
+        return user128pxKayitOl;
+    }
     
     //getter
 
