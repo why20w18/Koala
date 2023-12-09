05a14e404cc2dbd1f8e9b844be324c105f98826a
why20w18 <enco3162@gmail.com>
2023-12-09 17:53:13 +0300

    mail paketinden sonra eski haline cevrildi

--- src/veriTabaniPaketi/islemlerKullaniciDB.java
+++ src/veriTabaniPaketi/islemlerKullaniciDB.java
@@ -1,9 +1,36 @@
 
 package veriTabaniPaketi;
+
+import java.util.concurrent.ExecutionException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.mail.MessagingException;
+import javax.swing.SwingWorker;
+
+
+
+
 /**
  *
  * @author bvos
  */
 public class islemlerKullaniciDB {
+    //bu classi girisEkraninda tanýmladým <-> islemMail classiniysa burada tanimlayacagim
+    //zincir seklinde birbirine baglanarak gidecek
+    //islemMail mail = new islemMail();
+ 
     
+    private String mailAtilacakPosta = "";
+    private String unutulanPass = "";    
+    
+   public void kullaniciDBcek(String kullaniciAdi){ //hangi kullanicinin maili ve sifresi
+       this.mailAtilacakPosta = "bugrakaa06@gmail.com";
+       this.unutulanPass = "123456";
+   }
+   
+   
 }
+
+    
+    
+
