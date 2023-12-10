74842783df9a36734af09cecb44ead63637f1b14
why20w18 <enco3162@gmail.com>
2023-12-10 14:09:28 +0300

    kayit butonuna ozel islemDB uzerinden ekleme yapabilme metodu yaziliyor HATALI

---
+++ src/girisKaynakKODislemler/islemGiris.java
@@ -0,0 +1,57 @@
+package girisKaynakKODislemler;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+public class islemGiris {
+    
+    private JTextField jTextField_KullanýcýAdý;
+    private JPasswordField jPasswordField_Sifre;
+    
+    private String girilenKullanýcýAdý;
+    private String girilenSifre;
+    
+    // Deðiþkenlerimiz - Mesaj
+    private String sonucMesaj;
+        
+    private String sonucKurtarma_BosAlan;
+    private String sonucKurtarma_Mesaj;
+    
+    public islemGiris(JTextField jTextField_KullanýcýAdý, JPasswordField jPasswordField_Sifre) {
+        this.jTextField_KullanýcýAdý = jTextField_KullanýcýAdý;
+        this.jPasswordField_Sifre = jPasswordField_Sifre;
+        
+        // Bu deðerlerin her metod çaðrýldýðýnda güncellenmesi gerekir.
+        this.girilenKullanýcýAdý = jTextField_KullanýcýAdý.getText();
+        this.girilenSifre = new String(jPasswordField_Sifre.getPassword());
+    }
+    
+    // Diðer metodlar burada...
+    
+    // Güncellenmiþ metod
+    public boolean bosGirisKontrol(){
+        // Her metod çaðrýldýðýnda güncel deðerleri al.
+        this.girilenKullanýcýAdý = jTextField_KullanýcýAdý.getText();
+        this.girilenSifre = new String(jPasswordField_Sifre.getPassword());
+    
+        if(girilenKullanýcýAdý.equals("") && girilenSifre.equals("")){
+            this.sonucMesaj = "Kullanýcý Adý ve Þifresi Giriniz.";
+            return false;
+        }
+        else if(girilenKullanýcýAdý.equals("")){
+            this.sonucMesaj = "Kullanýcý Adý Giriniz.";
+            return false;
+        }
+        else if(girilenSifre.equals("")){
+            this.sonucMesaj = "Þifre Giriniz";
+            return false;
+        }
+        else {
+            this.sonucMesaj = "Baþarýlý";
+            return true;
+        }
+    }
+    
+    public String getSonucMesaj() {
+        return sonucMesaj;
+    }
+}
