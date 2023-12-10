45154bed9e0acf61160c2ff1a57943a2324519ef
why20w18 <enco3162@gmail.com>
2023-12-10 16:03:59 +0300

    kayit kontrol icin olusturuldu

---
+++ src/girisKaynakKODislemler/islemKayit.java
@@ -0,0 +1,58 @@
+package girisKaynakKODislemler;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+public class islemKayit{
+    
+    /*private JTextField jTextField_KayitKullaniciAdi;
+    private JTextField jTextField_Kayýt_Email;
+    private JPasswordField jPasswordField_kayitParola;
+    
+    private String KayitgirilenKullanýcýAdý;
+    private String KayitgirilenEMail;
+    private String KayitgirilenSifre;
+    
+    private String sonucMesaj;
+        
+   
+    
+    public islemKayit(JTextField jTextField_KayitKullaniciAdi,JTextField jTextField_Kayit_Email, JPasswordField jPasswordField_kayitParola) {
+        this.jTextField_KayitKullaniciAdi = jTextField_KayitKullaniciAdi;
+        this.jPasswordField_kayitParola = jPasswordField_kayitParola;
+        this.jTextField_Kayit_Email = jTextField_Kayit_Email;
+        
+        // Bu deðerlerin her metod çaðrýldýðýnda güncellenmesi gerekir.
+        this.girilenKullanýcýAdý = jTextField_KullanýcýAdý.getText();
+        this.girilenSifre = new String(jPasswordField_Sifre.getPassword());
+    }
+    
+    // Diðer metodlar burada...
+    
+    // Güncellenmiþ metod
+    public boolean bosGirisKontrol(){
+        // Her metod çaðrýldýðýnda güncel deðerleri al.
+        this.KayitgirilenKullanýcýAdý = KayitgirilenKullanýcýAdý.getText();
+        this.KayitgirilenSifre = new String(KayitgirilenSifre.getPassword());
+    
+        if(girilenKullanýcýAdý.equals("") && girilenSifre.equals("")){
+            this.sonucMesaj = "Kullanýcý Adý ve Þifresi Giriniz.";
+            return false;
+        }
+        else if(girilenKullanýcýAdý.equals("")){
+            this.sonucMesaj = "Kullanýcý Adý Giriniz.";
+            return false;
+        }
+        else if(girilenSifre.equals("")){
+            this.sonucMesaj = "Þifre Giriniz";
+            return false;
+        }
+        else {
+            this.sonucMesaj = "Baþarýlý";
+            return true;
+        }
+    }
+    
+    public String getSonucMesaj() {
+        return sonucMesaj;
+    }*/
+}
