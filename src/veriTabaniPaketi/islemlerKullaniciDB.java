ae21e013d2069177c2eca83dd29851826825b752
why20w18 <enco3162@gmail.com>
2023-12-10 20:01:05 +0300

    kullanici giris yaparken olusturulan objeye degerler ataniyor db kontrol ediliyor

--- src/veriTabaniPaketi/islemlerKullaniciDB.java
+++ src/veriTabaniPaketi/islemlerKullaniciDB.java
@@ -1,11 +1,14 @@
 package veriTabaniPaketi;
 
+import girisEkraniPaketi.girisEkrani;
+import girisKaynakKODislemler.islemGiris;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.LinkedList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.mail.MessagingException;
+import javax.swing.JTextField;
 import veriKullaniciPaketi.kullanici;
 
 public class islemlerKullaniciDB extends SQLBaglanti{
@@ -23,34 +26,35 @@ public class islemlerKullaniciDB extends SQLBaglanti{
             
     
     
-    public kullanici kullaniciBulDB(String girilenKullaniciAdi , String girilenKullaniciParola) throws SQLException{
-        kullanici kullanici;
+   public kullanici kullaniciBulDB(String girilenKullaniciAdi, String girilenKullaniciParola) throws SQLException {
+    kullanici kullanici = null;
+
+    komuttamamlayýcý = baglantý.prepareStatement(KULLANICI_ADI_SIFRESI);
+    komuttamamlayýcý.setString(1, girilenKullaniciAdi);
+    komuttamamlayýcý.setString(2, girilenKullaniciParola);
+
+    ResultSet sonuc = komuttamamlayýcý.executeQuery();
+
+    while (sonuc.next()) {
+        kullanici = new kullanici( //basta null olusturdugum kullanici constructorinin icini dolduruyorum veritabanindaki degerlerle
+            sonuc.getInt("id"),
+            sonuc.getString("kullaniciAdi"),
+            sonuc.getString("kullaniciParola"),
+            sonuc.getString("kullaniciMail"),
+            sonuc.getInt("toplamSure")
+                                     );
         
-        komuttamamlayýcý = baglantý.prepareStatement(KULLANICI_ADI_SIFRESI);
-        komuttamamlayýcý.setString(1, girilenKullaniciAdi);
-        komuttamamlayýcý.setString(2, girilenKullaniciParola);
-        //en bastaki soru isaretlerini ayarladik index 1 baslar 
-        
-        ResultSet sonuc = komuttamamlayýcý.executeQuery();
-        
-        while(sonuc.next()){
-            System.out.println("id:"+sonuc.getInt("id"));
-            System.out.println("Kullanici Adi:"+sonuc.getString("kullaniciAdi"));
-            System.out.println("Parola:"+sonuc.getString("kullaniciParola"));
-            System.out.println("Email:"+sonuc.getString("kullaniciMail"));
-            System.out.println("Toplam Sure:"+sonuc.getInt("toplamSure"));
-            
-        }
-        
-        
-        //veritabanindan donen degerleri resultSet classi ile tutabiliriz
-        //yukaridan kacinci soru isaretine denk geliyorsa onu yazacagiz sayma sayisi olarak baslar soru isaretleri
-        //databasedeki id ile arayacaksak string degil setInt diyerek arama yapariz
-        
-        kullanici = new kullanici(0,"bug", "12345", "bug@bug.com", 1);
-        
-        return kullanici;
+        // Ýstediðiniz diðer iþlemleri yapabilirsiniz
+        System.out.println("id:" + sonuc.getInt("id"));
+        System.out.println("Kullanici Adi:" + sonuc.getString("kullaniciAdi"));
+        System.out.println("Parola:" + sonuc.getString("kullaniciParola"));
+        System.out.println("Email:" + sonuc.getString("kullaniciMail"));
+        System.out.println("Toplam Sure:" + sonuc.getInt("toplamSure"));
     }
+
+    return kullanici;
+}
+
     
     //PreparedStatement komuttamamlayýcý; SQLBaglanti classinda tanimlandi ve kalitiliyor
      private final String KULLANICI_EKLE = "INSERT INTO kullanici (kullaniciAdi, kullaniciParola, kullaniciMail, toplamSure) VALUES (?, ?, ?, ?)";
