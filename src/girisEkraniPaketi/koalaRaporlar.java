fac6f624d1f28b24ac1a6777baaa0d46e7f643f7
why20w18 <enco3162@gmail.com>
2023-12-11 18:03:33 +0300

    jtable eklendi ve duzenlendi cikis islemleri halledildi

--- src/girisEkraniPaketi/koalaRaporlar.java
+++ src/girisEkraniPaketi/koalaRaporlar.java
@@ -4,18 +4,32 @@
  */
 package girisEkraniPaketi;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import javax.swing.JFrame;
+import veriKullaniciPaketi.kullanici;
+
 /**
  *
  * @author bvos
  */
 public class koalaRaporlar extends javax.swing.JFrame {
+    
+    kullanici kullanici2;
+    Dimension boyut = Toolkit.getDefaultToolkit().getScreenSize();
 
-    /**
-     * Creates new form koalaRaporlar
-     */
+
     public koalaRaporlar() {
         initComponents();
+        this.setLocation(boyut.width/2 - this.getSize().width / 2,boyut.height/2 - this.getSize().height / 2);
+
     }
+     public koalaRaporlar(kullanici kullanici) {
+         initComponents();
+         this.setLocation(boyut.width/2 - this.getSize().width / 2,boyut.height/2 - this.getSize().height / 2);
+       kullanici2 = new kullanici(kullanici.getId(), kullanici.getKullaniciAdi(), kullanici.getKullaniciParola(), kullanici.getKullaniciMail(), kullanici.getToplamSure());
+       //setTitle("koala App | Kullanýcý : " + kullanici2.getKullaniciAdi()+" | Toplam Süre : "+kullanici2.getToplamSure()+" Dakika | Raporlar Sekmesi");
+     }
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -26,21 +40,123 @@ public class koalaRaporlar extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel_RaporSekmesi = new javax.swing.JPanel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+        jButton_zamanMenusuDonus = new javax.swing.JButton();
+        jButton_dýþaAktar = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        setUndecorated(true);
+        setResizable(false);
+        addWindowListener(new java.awt.event.WindowAdapter() {
+            public void windowClosed(java.awt.event.WindowEvent evt) {
+                formWindowClosed(evt);
+            }
+            public void windowClosing(java.awt.event.WindowEvent evt) {
+                formWindowClosing(evt);
+            }
+        });
+
+        jPanel_RaporSekmesi.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel_RaporSekmesi.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 102, 0), 5));
+
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Tarihler", "Günler", "Toplam Süre (Dakika)", "Kendinize Puanýnýz (1-5 Arasý)"
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.String.class, java.lang.String.class, java.lang.Integer.class, java.lang.String.class
+            };
+            boolean[] canEdit = new boolean [] {
+                false, false, false, true
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        jScrollPane1.setViewportView(jTable1);
+
+        jButton_zamanMenusuDonus.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jButton_zamanMenusuDonus.setForeground(new java.awt.Color(0, 0, 153));
+        jButton_zamanMenusuDonus.setText("koala Çalýþma Alanýna Geri Dön");
+        jButton_zamanMenusuDonus.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton_zamanMenusuDonusActionPerformed(evt);
+            }
+        });
+
+        jButton_dýþaAktar.setFont(new java.awt.Font("Segoe UI", 1, 15)); // NOI18N
+        jButton_dýþaAktar.setForeground(new java.awt.Color(0, 102, 51));
+        jButton_dýþaAktar.setText("Dýþa Aktar");
+
+        javax.swing.GroupLayout jPanel_RaporSekmesiLayout = new javax.swing.GroupLayout(jPanel_RaporSekmesi);
+        jPanel_RaporSekmesi.setLayout(jPanel_RaporSekmesiLayout);
+        jPanel_RaporSekmesiLayout.setHorizontalGroup(
+            jPanel_RaporSekmesiLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel_RaporSekmesiLayout.createSequentialGroup()
+                .addGroup(jPanel_RaporSekmesiLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel_RaporSekmesiLayout.createSequentialGroup()
+                        .addComponent(jButton_zamanMenusuDonus, javax.swing.GroupLayout.PREFERRED_SIZE, 608, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 0, 0)
+                        .addComponent(jButton_dýþaAktar)))
+                .addGap(0, 0, Short.MAX_VALUE))
+        );
+        jPanel_RaporSekmesiLayout.setVerticalGroup(
+            jPanel_RaporSekmesiLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel_RaporSekmesiLayout.createSequentialGroup()
+                .addGroup(jPanel_RaporSekmesiLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton_zamanMenusuDonus)
+                    .addComponent(jButton_dýþaAktar))
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 259, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, 0))
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel_RaporSekmesi, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel_RaporSekmesi, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGap(0, 0, 0))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void formWindowClosed(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowClosed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_formWindowClosed
+
+    private void formWindowClosing(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowClosing
+        // TODO add your handling code here:
+    }//GEN-LAST:event_formWindowClosing
+
+    private void jButton_zamanMenusuDonusActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton_zamanMenusuDonusActionPerformed
+  
+        this.setVisible(false);
+    }//GEN-LAST:event_jButton_zamanMenusuDonusActionPerformed
 
     /**
      * @param args the command line arguments
@@ -78,5 +194,10 @@ public class koalaRaporlar extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton_dýþaAktar;
+    private javax.swing.JButton jButton_zamanMenusuDonus;
+    private javax.swing.JPanel jPanel_RaporSekmesi;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable jTable1;
     // End of variables declaration//GEN-END:variables
 }
