d8d501fb625d8dff9352bccd4c380aabc8d5d254
why20w18 <enco3162@gmail.com>
2023-12-11 19:01:47 +0300

    genel siralama ekrani olusturuldu 1 buton eklendi

--- src/girisEkraniPaketi/koalaGenelSiralama.java
+++ src/girisEkraniPaketi/koalaGenelSiralama.java
@@ -4,18 +4,36 @@
  */
 package girisEkraniPaketi;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import veriKullaniciPaketi.kullanici;
+
 /**
  *
  * @author bvos
  */
 public class koalaGenelSiralama extends javax.swing.JFrame {
-
-    /**
-     * Creates new form koalaGenelSiralama
-     */
+    
+    kullanici kullanici2;
+    Dimension boyut = Toolkit.getDefaultToolkit().getScreenSize();
+    
+    
     public koalaGenelSiralama() {
         initComponents();
+        this.setLocation(boyut.width/2 - this.getSize().width / 2,boyut.height/2 - this.getSize().height / 2);
+
     }
+    
+    public koalaGenelSiralama(kullanici kullanici) {
+        initComponents();
+        this.setLocation(boyut.width/2 - this.getSize().width / 2,boyut.height/2 - this.getSize().height / 2);
+            
+       kullanici2 = new kullanici(kullanici.getId(), kullanici.getKullaniciAdi(), kullanici.getKullaniciParola(), kullanici.getKullaniciMail(), kullanici.getToplamSure());
+       setTitle("koala App | Kullanýcý : " + kullanici2.getKullaniciAdi()+" | Toplam Süre : "+kullanici2.getToplamSure()+" Dakika | Genel Sýralama Sekmesi");
+     
+    }
+    
+    
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -26,17 +44,40 @@ public class koalaGenelSiralama extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        jPanel1 = new javax.swing.JPanel();
+        jButton1 = new javax.swing.JButton();
+
+        jPanel1.setBackground(new java.awt.Color(51, 204, 0));
+        jPanel1.setPreferredSize(new java.awt.Dimension(645, 340));
+
+        jButton1.setText("Geri Dön");
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(277, 277, 277)
+                .addComponent(jButton1)
+                .addContainerGap(286, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jButton1)
+                .addContainerGap(318, Short.MAX_VALUE))
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 640, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 347, Short.MAX_VALUE)
         );
 
         pack();
@@ -78,5 +119,7 @@ public class koalaGenelSiralama extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
